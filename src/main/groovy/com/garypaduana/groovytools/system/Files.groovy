f6bf7afc0a9fa4159d6fc69888e5eaed2d062023
Gary Paduana <gary.paduana@gmail.com>
2017-02-05 13:34:50 -0800

    add hashing service for files and directories

--- src/main/groovy/com/garypaduana/groovytools/system/Files.groovy
+++ src/main/groovy/com/garypaduana/groovytools/system/Files.groovy
@@ -3,6 +3,7 @@ package com.garypaduana.groovytools.system
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import java.security.MessageDigest
 
 class Files{
 
@@ -16,6 +17,50 @@ class Files{
                 }
             }
         }
+    }
+
+    static def generateDigest(File file, String digest, int paddedLength){
+        MessageDigest md = MessageDigest.getInstance(digest)
+        md.reset()
+        def files = []
+        def directories = []
+
+        if(file.isDirectory()){
+            file.eachFileRecurse(){sf ->
+                if(sf.isFile()){
+                    files.add(sf)
+                }
+                else{
+                    directories.add(file.toURI().relativize(sf.toURI()).toString())
+                }
+            }
+        }
+        else if(file.isFile()){
+            files.add(file)
+        }
+
+        files.sort({a, b -> return a.getAbsolutePath() <=> b.getAbsolutePath()})
+        directories.sort()
+
+        files.each(){f ->
+            println file.toURI().relativize(f.toURI()).toString()
+            f.withInputStream(){is ->
+                byte[] buffer = new byte[8192]
+                int read = 0
+                while((read = is.read(buffer)) > 0){
+                    md.update(buffer, 0, read)
+                }
+            }
+        }
+
+        directories.each(){d ->
+            println d
+            md.update(d.getBytes())
+        }
+
+        byte[] digestBytes = md.digest()
+        BigInteger bigInt = new BigInteger(1, digestBytes)
+        return bigInt.toString(16).padLeft(paddedLength, '0')
     }
 
     static def findImagesLargerThan(File rootDir, int width, int height){
--- src/main/test/com/garypaduana/groovytools/system/TestFiles.groovy
+++ src/main/test/com/garypaduana/groovytools/system/TestFiles.groovy
@@ -15,4 +15,26 @@ class TestFiles extends Specification {
             }
             results.size() == 1
     }
+
+    /**
+     * Sample test data taken from:
+     * http://www.nsrl.nist.gov/testdata/
+     */
+    def "test hash single file"(){
+        setup:
+            def source = new File("src/main/resources/test-data/hash-sample")
+        when:
+            def result = Files.generateDigest(source, "SHA-256", 64)
+        then:
+            result.equalsIgnoreCase("248D6A61D20638B8E5C026930C3E6039A33CE45964FF2167F6ECEDD419DB06C1")
+    }
+
+    def "test hash directory"(){
+        setup:
+            def source = new File("src/main/resources/test-data")
+        when:
+            def result = Files.generateDigest(source, "SHA-256", 64)
+        then:
+            result.equalsIgnoreCase("b1e430c0e26df940c9a31fcd5280857fcbc28e491f157fd7c3bbfce0d3f8058c")
+    }
 }
